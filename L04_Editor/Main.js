2b191e3595cd8527e64dd19192786f6516be83f2
Jonathan <jonathan.rissler@gmx.de>
2021-05-06 17:32:23 +0200

    fix pick & release

--- L04_Editor/Main.js
+++ L04_Editor/Main.js
@@ -9,9 +9,14 @@ var L04_Editor;
     let viewport;
     let player;
     let cmpCamera;
-    let speed = 5;
-    let rotate = 2.5;
+    let ball;
+    const speed = 5;
+    const rotate = 2.5;
+    const pickReleaseCooldown = 500;
     let forward;
+    let playerItem;
+    let canPickItem = true;
+    let hasItem = false;
     async function init() {
         await f.Project.loadResourcesFromHTML();
         root = f.Project.resources["Graph|2021-04-27T14:37:42.239Z|64317"];
@@ -19,20 +24,25 @@ var L04_Editor;
         f.Physics.settings.defaultRestitution = 0.3;
         f.Physics.settings.defaultFriction = 0.8;
         cmpCamera = new f.ComponentCamera();
-        cmpCamera.mtxPivot.translate(ƒ.Vector3.ONE(30));
-        cmpCamera.mtxPivot.lookAt(f.Vector3.ZERO());
+        // cmpCamera.mtxPivot.translate(ƒ.Vector3.ONE(3));
+        // cmpCamera.mtxPivot.lookAt(f.Vector3.ZERO());
+        cmpCamera.mtxPivot.translateY(1);
+        cmpCamera.mtxPivot.rotateX(15);
+        cmpCamera.mtxPivot.rotateY(0);
+        cmpCamera.mtxPivot.translateZ(-6);
         let canvas = document.querySelector("canvas");
         viewport = new f.Viewport();
         viewport.initialize("InteractiveViewport", root, cmpCamera, canvas);
         createPlayer();
         createRigidbodies();
         f.Physics.start(root);
-        // f.Physics.settings.debugDraw = true;
+        f.Physics.settings.debugDraw = true;
         f.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
         f.Loop.start(); //Stard the game loop
     }
     function update() {
         movePlayer();
+        hndItem();
         f.Physics.world.simulate(f.Loop.timeFrameReal / 1000);
         viewport.draw();
     }
@@ -45,6 +55,9 @@ var L04_Editor;
         player.addComponent(new f.ComponentTransform());
         player.mtxLocal.translateY(5);
         player.addComponent(cmpPlayer);
+        player.addComponent(cmpCamera);
+        playerItem = new f.Node("PlayerItem");
+        player.addChild(playerItem);
         root.appendChild(player);
     }
     function createRigidbodies() {
@@ -54,7 +67,7 @@ var L04_Editor;
             // node.getComponent(f.ComponentMesh).mtxPivot.scaling = f.Vector3.ONE();
             node.addComponent(new f.ComponentRigidbody(0, f.PHYSICS_TYPE.STATIC, f.COLLIDER_TYPE.CUBE, f.PHYSICS_GROUP.DEFAULT));
         }
-        let ball = root.getChildrenByName("ball")[0];
+        ball = root.getChildrenByName("ball")[0];
         // ball.mtxLocal.scaling = ball.getComponent(f.ComponentMesh).mtxPivot.scaling;
         // ball.getComponent(f.ComponentMesh).mtxPivot.scaling = f.Vector3.ONE();
         ball.addComponent(new f.ComponentRigidbody(1, f.PHYSICS_TYPE.DYNAMIC, f.COLLIDER_TYPE.CUBE, f.PHYSICS_GROUP.DEFAULT));
@@ -70,7 +83,41 @@ var L04_Editor;
             cmpPlayer.rotateBody(ƒ.Vector3.Y(rotate));
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT]))
             cmpPlayer.rotateBody(ƒ.Vector3.Y(-rotate));
-        cmpCamera.mtxPivot = player.mtxLocal;
+        // cmpCamera.mtxPivot = player.mtxLocal;
+    }
+    function hndItem() {
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.E])) {
+            if (hasItem) {
+                releaseItem();
+                return;
+            }
+            if (!canPickItem)
+                return;
+            pickItem();
+            f.Time.game.setTimer(pickReleaseCooldown, 1, () => canPickItem = true);
+        }
+    }
+    function releaseItem() {
+        let item = playerItem.getChild(0);
+        playerItem.removeChild(item);
+        root.addChild(item);
+        ball.getComponent(f.ComponentRigidbody).physicsType = f.PHYSICS_TYPE.DYNAMIC;
+        hasItem = false;
+        canPickItem = false;
+        f.Time.game.setTimer(pickReleaseCooldown, 1, () => canPickItem = true);
+    }
+    function pickItem() {
+        let hitInfo = f.Physics.raycast(cmpPlayer.getPosition(), player.mtxWorld.getZ(), 2.5);
+        if (hitInfo.hit) {
+            canPickItem = false;
+            f.Time.game.setTimer(pickReleaseCooldown, 1, () => canPickItem = true);
+            if (hitInfo.rigidbodyComponent.getContainer() === ball) {
+                ball.getComponent(f.ComponentRigidbody).physicsType = f.PHYSICS_TYPE.KINEMATIC;
+                playerItem.addChild(ball);
+                ball.mtxLocal.set(f.Matrix4x4.TRANSLATION(f.Vector3.Z(2)));
+                f.Time.game.setTimer(pickReleaseCooldown, 1, () => hasItem = true);
+            }
+        }
     }
 })(L04_Editor || (L04_Editor = {}));
 //# sourceMappingURL=Main.js.map
--- L04_Editor/Main.ts
+++ L04_Editor/Main.ts
@@ -9,10 +9,15 @@ namespace L04_Editor {
     let viewport: f.Viewport;
     let player: f.Node;
     let cmpCamera: f.ComponentCamera;
+    let ball: f.Node;
 
-    let speed: number = 5;
-    let rotate: number = 2.5;
+    const speed: number = 5;
+    const rotate: number = 2.5;
+    const pickReleaseCooldown: number = 500;
     let forward: ƒ.Vector3;
+    let playerItem: f.Node;
+    let canPickItem: boolean = true;
+    let hasItem: boolean = false;
 
     async function init(): Promise<void> {
         await f.Project.loadResourcesFromHTML();
@@ -22,8 +27,12 @@ namespace L04_Editor {
         f.Physics.settings.defaultRestitution = 0.3;
         f.Physics.settings.defaultFriction = 0.8;
         cmpCamera = new f.ComponentCamera();
-        cmpCamera.mtxPivot.translate(ƒ.Vector3.ONE(30));
-        cmpCamera.mtxPivot.lookAt(f.Vector3.ZERO());
+        // cmpCamera.mtxPivot.translate(ƒ.Vector3.ONE(3));
+        // cmpCamera.mtxPivot.lookAt(f.Vector3.ZERO());
+        cmpCamera.mtxPivot.translateY(1);
+        cmpCamera.mtxPivot.rotateX(15);
+        cmpCamera.mtxPivot.rotateY(0);
+        cmpCamera.mtxPivot.translateZ(-6);
         let canvas: HTMLCanvasElement = document.querySelector("canvas");
         viewport = new f.Viewport();
         viewport.initialize("InteractiveViewport", root, cmpCamera, canvas);
@@ -32,16 +41,15 @@ namespace L04_Editor {
         createRigidbodies();
 
         f.Physics.start(root);
-        // f.Physics.settings.debugDraw = true;
+        f.Physics.settings.debugDraw = true;
 
         f.Loop.addEventListener(f.EVENT.LOOP_FRAME, update);
         f.Loop.start(); //Stard the game loop
     }
 
     function update(): void {
-        
-        
         movePlayer();
+        hndItem();
         f.Physics.world.simulate(f.Loop.timeFrameReal / 1000);
         viewport.draw();
     }
@@ -57,19 +65,22 @@ namespace L04_Editor {
         player.addComponent(new f.ComponentTransform());
         player.mtxLocal.translateY(5);
         player.addComponent(cmpPlayer);
+        player.addComponent(cmpCamera);
+        playerItem = new f.Node("PlayerItem");
+        player.addChild(playerItem);
         root.appendChild(player);
     }
 
     function createRigidbodies(): void {
         let level: f.Node = root.getChildrenByName("level")[0];
-        
+
         for (let node of level.getChildren()) {
             // node.mtxLocal.scaling = node.getComponent(f.ComponentMesh).mtxPivot.scaling;
             // node.getComponent(f.ComponentMesh).mtxPivot.scaling = f.Vector3.ONE();
             node.addComponent(new f.ComponentRigidbody(0, f.PHYSICS_TYPE.STATIC, f.COLLIDER_TYPE.CUBE, f.PHYSICS_GROUP.DEFAULT));
-            
+
         }
-        let ball: f.Node = root.getChildrenByName("ball")[0];
+        ball = root.getChildrenByName("ball")[0];
         // ball.mtxLocal.scaling = ball.getComponent(f.ComponentMesh).mtxPivot.scaling;
         // ball.getComponent(f.ComponentMesh).mtxPivot.scaling = f.Vector3.ONE();
         ball.addComponent(new f.ComponentRigidbody(1, f.PHYSICS_TYPE.DYNAMIC, f.COLLIDER_TYPE.CUBE, f.PHYSICS_GROUP.DEFAULT));
@@ -91,6 +102,44 @@ namespace L04_Editor {
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT]))
             cmpPlayer.rotateBody(ƒ.Vector3.Y(-rotate));
 
-        cmpCamera.mtxPivot = player.mtxLocal;
+        // cmpCamera.mtxPivot = player.mtxLocal;
+    }
+
+    function hndItem(): void {
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.E])) {
+            if (hasItem) {
+                releaseItem();
+                return;
+            }
+
+            if (!canPickItem) return;
+
+            pickItem();
+            f.Time.game.setTimer(pickReleaseCooldown, 1, () => canPickItem = true);
+        }
+    }
+
+    function releaseItem(): void {
+        let item: f.Node = playerItem.getChild(0);
+        playerItem.removeChild(item);
+        root.addChild(item);
+        ball.getComponent(f.ComponentRigidbody).physicsType  = f.PHYSICS_TYPE.DYNAMIC;
+        hasItem = false;
+        canPickItem = false;
+        f.Time.game.setTimer(pickReleaseCooldown, 1, () => canPickItem = true);
+    }
+
+    function pickItem(): void {
+        let hitInfo: f.RayHitInfo = f.Physics.raycast(cmpPlayer.getPosition(), player.mtxWorld.getZ(), 2.5);
+        if (hitInfo.hit) {
+            canPickItem = false;
+            f.Time.game.setTimer(pickReleaseCooldown, 1, () => canPickItem = true);
+            if (hitInfo.rigidbodyComponent.getContainer() === ball) {
+                ball.getComponent(f.ComponentRigidbody).physicsType  = f.PHYSICS_TYPE.KINEMATIC;
+                playerItem.addChild(ball);
+                ball.mtxLocal.set(f.Matrix4x4.TRANSLATION(f.Vector3.Z(2)));
+                f.Time.game.setTimer(pickReleaseCooldown, 1, () => hasItem = true);
+            }
+        }
     }
 }
