d221d342492deb9fb67dff6d49b1fc61eb141c6d
Jonathan <jonathan.rissler@gmx.de>
2021-07-21 09:58:08 +0200

    Add controls

--- Endabgabe/LevelSelect/LevelSelect.js
+++ Endabgabe/LevelSelect/LevelSelect.js
@@ -2,6 +2,7 @@
 var Endabgabe;
 (function (Endabgabe) {
     window.addEventListener("load", init);
+    let isControlsVisible = false;
     async function init() {
         let level1 = document.getElementById("level1");
         level1.addEventListener("click", () => clickLevel(1));
@@ -13,9 +14,19 @@ var Endabgabe;
         level4.addEventListener("click", () => clickLevel(4));
         let level5 = document.getElementById("level5");
         level5.addEventListener("click", () => clickLevel(5));
+        let howToPlay = document.getElementById("howToPlay");
+        howToPlay.addEventListener("click", hndHowToPlay);
     }
     function clickLevel(_level) {
         window.location.href = "../Endabgabe.html?id=" + _level;
+    }
+    function hndHowToPlay() {
+        isControlsVisible = !isControlsVisible;
+        let controls = document.getElementById("controls");
+        if (isControlsVisible)
+            controls.style.visibility = "hidden";
+        else
+            controls.style.visibility = "visible";
     }
 })(Endabgabe || (Endabgabe = {}));
 //# sourceMappingURL=LevelSelect.js.map
